31945fbe12809a8ed2e3501ce7a050fc62693c56
Björn Petersen <git@bjoernpetersen.net>
2022-09-12 12:55:12 +0200

    Implement idol rule

--- bot/event/rules/idol.py
+++ bot/event/rules/idol.py
@@ -1,4 +1,29 @@
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass
+
+from bot import telegram
 from bot.event.rule import EventRule
+
+_LOG = logging.getLogger(__name__)
+
+
+@dataclass
+class HoroscopeEvent:
+    chat_id: int
+    message_id: int
+    user_id: int
+    horoscope: str
+
+    @classmethod
+    def deserialize(cls, event: dict) -> HoroscopeEvent:
+        return cls(
+            chat_id=event["chat_id"],
+            message_id=event["message_id"],
+            user_id=event["user_id"],
+            horoscope=event["horoscope"],
+        )
 
 
 class IdolRule(EventRule):
@@ -8,4 +33,13 @@ class IdolRule(EventRule):
         pass
 
     def __call__(self, event: dict) -> bool:
-        return False
+        data = HoroscopeEvent.deserialize(event)
+        if "Idol" not in data.horoscope:
+            _LOG.debug("No idols found, skipping")
+            return True
+
+        telegram.send_message(
+            chat_id=data.chat_id,
+            text="Balek?!",
+            reply_to_message_id=data.message_id,
+        )
