6a200e7dc4db0c57a404daebb3a822cc5f01f30a
Björn Petersen <git@bjoernpetersen.net>
2023-04-03 10:23:43 +0200

    Fix resource name

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,7 +3,7 @@ resource "google_service_account" "service_account" {
   display_name = "Moderator Bot"
 }
 
-resource "google_project_iam_member" "service_account_publisher" {
+resource "google_project_iam_member" "service_account_consumer" {
   project = google_service_account.service_account.project
   role    = "projects/prep-telegram-bots/roles/pubsubConsumer"
   member  = "serviceAccount:${google_service_account.service_account.email}"
